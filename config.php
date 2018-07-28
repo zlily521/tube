7863f42c191fcffe33f01f85df595de33ca84af1
zlily521 <zlily988@gmail.com>
2018-07-28 23:34:53 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyDurbxz386ANayG3jlcULz5aQgXsWMZSdY');
-define('GJ_CODE', 'HK');
+define('GJ_CODE', 'TW');
 define('SITE_NAME', 'YoKu');
 define('TITLENAME', 'YoKu');
 define('EN2DEKEY', '?]jgCzq}sdH`F`3Op');
