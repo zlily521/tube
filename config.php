8efde46a58043af4a2f01e568ce3a9599c2dacc5
zlily521 <zlily988@gmail.com>
2018-11-25 11:21:50 +0800

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyDurbxz386ANayG3jlcULz5aQgXsWMZSdY');
-define('GJ_CODE', 'SG');
-define('SITE_NAME', 'YoKu');
-define('TITLENAME', 'YoKu');
-define('EN2DEKEY', '?]jgCzq}sdH`F`3Op');
-define('EMAIL', 'zlily988@gmail.com');
-?>
